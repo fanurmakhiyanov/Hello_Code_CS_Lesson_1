1f4cecbd73364ba546bae1934aea83de435376fd
fanur <makhiyanov.ff@gmail.com>
2022-06-06 15:58:28 +0300

    Example 007 Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("*");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 50,
+    xc = 40, yc = 50;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
